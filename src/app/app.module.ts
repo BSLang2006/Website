b0dca99480101050f85723dc2efe7c71a7549619
Brandon <BrandonScottLang@gmail.com>
2023-09-18 13:35:41 -0400

    ready to start building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ResumeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
